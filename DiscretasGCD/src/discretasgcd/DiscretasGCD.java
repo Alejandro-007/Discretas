dc380c0d741017370127dd966165e2eb593e12c5
Alejandro-007 <106827646+Alejandro-007@users.noreply.github.com>
2022-07-25 12:44:32 -0500

    Update DiscretasGCD.java

--- DiscretasGCD/src/discretasgcd/DiscretasGCD.java
+++ DiscretasGCD/src/discretasgcd/DiscretasGCD.java
@@ -1,16 +1,23 @@
+
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    elabora un programa donde el usuario pueda ingresar dos numeros enteros pisitivos,
+    y se le permita hallar el GCD, utilizando el algoritmo de Euclides.
+
+            JOHAN SEBASTIAN HENAO CAÑAS
+            DOUGLAS LANNY ALARCÓN SALAZAR
+            ALEJANDRO BECERRA ACEVEDO
+
+            johan.henao1@udea.edu.co
+            douglas.alarcon@udea.edu.co
+            alejandro.becerraa@udea.edu.co
  */
+
+
 package discretasgcd;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Talia Palacio Mira
- */
+
 public class DiscretasGCD {
 
         public static void main(String[] args) {
@@ -25,14 +32,15 @@ public class DiscretasGCD {
                 a = Leer.nextInt();
                 System.out.println("        Ingrese el segundo numero");
                 b = Leer.nextInt();
-                
+
                 if (GCD(a,b) != 0){
-                    System.out.println("        El Maximo Comun Divisor (GCD) de los numeros ingresados es:  " + Math.abs(GCD(a,b)));
+                    System.out.println("        El Maximo Comun Divisor (GCD) de los numeros ingresados es:  " + GCD(a,b));
                     System.out.println("");  
                 }
+
             System.out.println("        Desea ingresar otro par de nuemros (y/n)");
             y = Leer.next(); 
-            
+
             if(!y.equals("y")){
                 sw = 0;                
             }
@@ -41,38 +49,50 @@ public class DiscretasGCD {
     }
     
     public static int GCD (int a,int b){
-        int gcd = 0, divisor = a,residuo,dividendo = b;
-       if (a == 0 && b == 0){
-           System.out.println("     Los dos valores NO pueden ser cero a la vez");
-           return 0;            
-       }else{
-           if (b != 0 ){
-                if (Math.abs(a) >= Math.abs(b)){
-
-                    do{            
-                        residuo = dividendo % divisor;
+        int gcd = 0, divisor = Math.abs(b),residuo,dividendo = Math.abs(a);
+        if (divisor == 0 && dividendo == 0){
+            System.out.println("     Los dos valores NO pueden ser cero a la vez");
+            return 0;            
+        }else{
+            if (dividendo != 0 && divisor !=0){
+                if (dividendo >= divisor){
+                    do{ 
+                        //residuo = dividendo % divisor;
+                        residuo = divide_unsigned(dividendo, divisor);
                         dividendo = divisor;
                         gcd = divisor;
                         divisor = residuo;     
 
                     }while (residuo != 0); 
-                    
-                }else{
-                    dividendo = a;
-                    divisor = b;
-                    do{            
-                        residuo = dividendo % divisor;
-                        dividendo = divisor;
-                        gcd = divisor;
-                        divisor = residuo;     
-
-                    }while (residuo != 0);
+                 }else{
+                     dividendo = Math.abs(b);
+                     divisor = Math.abs(a);
+                     do{            
+                         residuo = divide_unsigned(dividendo, divisor);
+                         dividendo = divisor;
+                         gcd = divisor;
+                         divisor = residuo;  
+                     }while (residuo != 0);
                 }           
-           }else{
-               gcd = a;
-           }
-       }
+            }else{
+                if (dividendo <= divisor){
+                    gcd = divisor;
+                 }else{
+                     divisor= Math.abs(a);
+                     dividendo = Math.abs(b);
+                     gcd = divisor;
+                }                
+            }
+        }
         return gcd;
     }
+    
+    static int divide_unsigned(int n, int d){
+        int r = n;
+        while (r >= d){
+            r = r-d;
+        }
+        return r;
+        }
     
 }
